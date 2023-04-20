ea24b1ec9c7f915a81bd627b62bda29433c1d2b9
AbriLu <108410162+AbriLu@users.noreply.github.com>
2023-04-20 20:25:00 +0200

    CSS and Html setup done

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiLink = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=512c662b2b23e7b6588a269bf32277c9&page=1';
+const imgPath = 'https://image.tmdb.org/t/p/w1280';
+const searchApi = "https://api.themoviedb.org/3/search/movie?&api_key=512c662b2b23e7b6588a269bf32277c9&query=";
